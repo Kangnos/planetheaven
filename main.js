7ad23a8e2c9c5478a140154d2a9a81d49b67935a
Kangnos <57825621+Kangnos@users.noreply.github.com>
2020-03-18 14:20:19 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+
+var app = http.createServer(function(request,response){
+    var _url = request.url;
+    var queryData = url.parse(_url, true).query;
+    var title = queryData.id;
+    if(_url == '/'){
+      _url = '/index.html';
+      title = 'Welcome';
+    }
+    if(_url == '/favicon.ico'){
+      return response.writeHead(404);
+    }
+    response.writeHead(200);
+    response.end(fs.readFileSync(__dirname + _url));
+});
+app.listen(3000);
